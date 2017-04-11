86b07e7c148b3ea20c32ffa1b1b69b39c1870865
himkt <himkt@klis.tsukuba.ac.jp>
2017-04-11 16:39:48 +0900

    Make package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bbot",
+    version="0.0.1",
+    author='himkt',
+    author_email='himkt@klis.tsukuba.ac.jp',
+    url='https://github.com/himkt/bulletin_bot',
+
+    packages=find_packages(),
+)
