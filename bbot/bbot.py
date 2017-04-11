1fc11954abea2f01d0289012c582370291838a55
himkt <himkt@klis.tsukuba.ac.jp>
2017-04-11 13:57:14 +0900

    [WIP] Impl bbot

---
+++ bbot/bbot.py
@@ -0,0 +1,59 @@
+from twins import Twins
+from pyquery import PyQuery as pq
+from time import sleep
+from random import random
+
+
+class BBot(Twins):
+
+    def __init__(self, username, password):
+        super(BBot, self).__init__(username, password)
+
+    def get_course_notifications(self):
+        target_list = ['学群授業', '大学院授業']
+
+        def is_target(d):
+            result = map(lambda t: d.text.startswith(t), target_list)
+            return any(result)
+
+        def get_attrib(d):
+            _raw = d.attrib['href'].split('?')[1]
+            return _raw.split('&')
+
+        def to_dict(attrib_list):
+            return dict(map(lambda attr: attr.split('='), attrib_list))
+
+        r = self.req("KJW0001100-flow")
+
+        for d in pq(r.text)("a"):
+
+            if d.text is None:
+                continue
+
+            if is_target(d):
+                attrib_list = get_attrib(d)
+                attrib_dict = to_dict(attrib_list)
+
+                self.req("KJW0001100-flow")
+                rr = self.get(attrib_dict)
+
+                for dd in pq(rr.text)("a"):
+                    title = dd.text
+
+                    dd_attrib_list = get_attrib(dd)
+                    dd_attrib_dict = to_dict(dd_attrib_list)
+
+                    if dd_attrib_dict['_eventId'] == 'confirm':
+                        rrr = self.get(dd_attrib_dict)
+                        body = pq(rrr.text)("div.keiji-naiyo")
+
+                        if title and body:
+                            print(title, body)
+
+                        span = random() * 5
+                        sleep(span)
+
+
+if __name__ == '__main__':
+    client = BBot('<your student id>', 'password')
+    client.get_course_notifications()
